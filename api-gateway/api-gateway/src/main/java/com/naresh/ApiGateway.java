483718b2797aadb48006faf59729bfbb5eca1a3a
naresh <nareshbabukalpana@gmail.com>
2026-03-10 09:53:17 +0530

    checking 2

--- api-gateway/api-gateway/src/main/java/com/naresh/ApiGateway.java
+++ api-gateway/api-gateway/src/main/java/com/naresh/ApiGateway.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 // naresh checking
+// naresh check 2
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGateway {
